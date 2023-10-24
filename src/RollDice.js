d49c780a773d36b181224ca75b43c004b5d087c3
UmitAygunn <umitaygun43@gmail.com>
2023-10-24 20:22:21 +0300

    first commit

---
+++ src/RollDice.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Dice from "./Dice.js";
+
+class RollDice extends Component {
+  static defaultProps = {
+    sides: ["one", "two", "three", "four", "five", "six"],
+  };
+
+  state = { dice1: "one", dice2: "two", rolling: false };
+  roll = () => {
+    const newDice1 =
+      this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+    const newDice2 =
+      this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+    this.setState({ dice1: newDice1, dice2: newDice2, rolling: true });
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 500);
+  };
+
+  render() {
+    return (
+      <div className="RollDice">
+        <div className="RollDice-container">
+          <Dice face={this.state.dice1} rolling={this.state.rolling} />
+          <Dice face={this.state.dice2} rolling={this.state.rolling} />
+        </div>
+        <button
+          className="btn"
+          onClick={this.roll}
+          disabled={this.state.rolling}
+        >
+          {this.state.rolling ? "Hooop..." : "Shake the Dice"}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default RollDice;
